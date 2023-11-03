55330d572b60c14b33ebae2ee45468101413f843
dannykay62 <danielkay62@gmail.com>
2023-11-03 01:39:36 +0100

    Implement a hash_password function that expects one string argument name password and returns a salted, hashed password, which is a byte string

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -2,10 +2,11 @@
 """ expects one string argument name password and returns a salted, hashed 
     password, which is a byte string
 """
-from bcrypt import hashpw, gensalt
 
 
 def hash_password(password: str) -> bytes:
     """Use the bcrypt package to perform the hashing (with hashpw)."""
+    from bcrypt import hashpw, gensalt
+
     password, salt = bytes(password.encode()), gensalt()
-    return hashpw(password, gensalt)
+    return hashpw(password, salt)

32e5eb8c25d450034c36d9b08b6f1d2795c618ed
dannykay62 <danielkay62@gmail.com>
2023-11-07 23:49:18 +0100

    Uploads...

--- 0x01-Basic_authentication/api/v1/views/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Doc views for the app"""
-from flask import Blueprint
-
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-
-from api.v1.views.index import *
-# from api.v1.views.users import *

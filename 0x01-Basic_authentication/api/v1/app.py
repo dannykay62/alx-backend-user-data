9ea9936e3e33dcc1531f3acb1678d248656857a8
dannykay62 <danielkay62@gmail.com>
2023-11-08 01:46:07 +0100

    validate all requests to secure the API

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,14 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+AUTH_TYPE = getenv("AUTH_TYPE")
+if AUTH_TYPE == 'auth':
+    from api.v1.auth.auth import auth
+    auth = Auth()
+# elif AUTH_TYPE == 'basic_auth':
+#     from api.v1.auth.basic_auth import BasicAuth
+#     auth = BasicAuth()
 
 
 @app.errorhandler(401)
@@ -31,6 +39,22 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.before_request
+def before_request() -> str:
+    """before request method handler"""
+    if auth is None:
+        return
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":

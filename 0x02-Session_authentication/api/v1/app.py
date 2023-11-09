b364b667a849f4e208aaa204bcc0e4239acd841b
dannykay62 <danielkay62@gmail.com>
2023-11-10 00:56:32 +0100

    add a new endpoint: GET /users/me to retrieve the authenticated User object

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -43,7 +43,7 @@ def not_found(error) -> str:
 
 @app.before_request
 def before_request() -> str:
-    """ method to handler before request """
+    """ method to handle before request """
     if auth is None:
         return
     excluded_paths = ['/api/v1/status/',
@@ -55,6 +55,7 @@ def before_request() -> str:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
+    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":

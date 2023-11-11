1b85999608bdacb156c4031d45c3073e25660913
dannykay62 <danielkay62@gmail.com>
2023-11-11 01:42:17 +0100

    Modified class

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -64,3 +64,33 @@ class BasicAuth(Auth):
         user_email, user_password = decoded_base64_authorization_header.split(
             ':', 1)
         return user_email, user_password
+    
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """
+        Get the User instance based on user email and password.
+
+        :param user_email: User email
+        :type user_email: str
+        :param user_pwd: User password
+        :type user_pwd: str
+        :return: User instance or None if not found or invalid credentials
+        :rtype: TypeVar('User')
+        """
+        if user_email is None or not isinstance(user_email, str) or user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        # Search for the user in the database (file) based on email
+        users = User.search({'email': user_email})
+
+        if not users:
+            # No user found with the given email
+            return None
+
+        # Assume the first user in the list is the one we're looking for
+        found_user = users[0]
+
+        # Check if the provided password is valid for the found user
+        if not found_user.is_valid_password(user_pwd):
+            return None
+
+        return found_user

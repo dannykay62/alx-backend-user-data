218590260c1154892749fef7012b70ba7c38ec0d
dannykay62 <danielkay62@gmail.com>
2023-11-11 15:36:56 +0100

    Modify the excluded paths

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -51,7 +51,8 @@ def before_request() -> str:
         return
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
     if auth.authorization_header(request) is None:
@@ -59,6 +60,8 @@ def before_request() -> str:
     if auth.current_user(request) is None:
         abort(403)
     request.current_user = auth.current_user(request)
+    if auth.authorization_header(request) and auth.session_cookie(request):
+        return None, abort(401)
 
 
 if __name__ == "__main__":

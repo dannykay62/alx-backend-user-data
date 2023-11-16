635a2503c525f2323affb0b13f63ac6f9da2a9d0
dannykay62 <danielkay62@gmail.com>
2023-11-16 15:29:16 +0100

    Modified the auth and app modules to implement functionalities to create a user, login, delete or logout a session and destroy a session

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -19,7 +19,7 @@ def users():
     email = request.form.get('email')
     password = request.form.get('password')
     try:
-        Auth.register_user(email, password)
+        AUTH.register_user(email, password)
         return jsonify({"email": email, "message": "user created"}), 200
     except Exception:
         return jsonify({"message": "email already registered"}), 400
@@ -31,14 +31,14 @@ def login():
     the form"""
     email = request.form.get('email')
     password = request.form.get('password')
-    if Auth.valid_login(email, password) is False:
+    if AUTH.valid_login(email, password) is False:
         abort(401)
-    session_id = Auth.create_session(email)
+    session_id = AUTH.create_session(email)
     response = jsonify({'email': email, 'message': 'logged in'})
     response.set_cookie('session_id', session_id)
     return response
 
-@app.route('/sessions', methods=['delete'], strict_slashes=False)
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
 def logout():
     """Find the user with the requested session ID. If the user exists destroy
     the session and redirect the user to GET /. If the user does not exist,
@@ -49,6 +49,15 @@ def logout():
         abort(403)
     AUTH.destroy_session(user.id)
     return redirect('/')
+
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile():
+    """Create user profile using the session_id cookie"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if session_id is None or user is None:
+        abort(403)
+    return jsonify({"email": user.email}), 200
 
 
 if __name__ == "__main__":

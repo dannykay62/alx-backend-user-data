68bc408263e22d9e6ee6314660eed9a3ab8b6489
dannykay62 <danielkay62@gmail.com>
2023-11-18 00:59:12 +0100

    Modified db module for corrections

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -42,7 +42,7 @@ class DB:
         """
         if kwargs is None:
             raise InvalidRequestError
-        user = self._session.query(User).filter_by(**kwargs).first
+        user = self._session.query(User).filter_by(**kwargs).first()
         if user is None:
             raise NoResultFound
         return user
